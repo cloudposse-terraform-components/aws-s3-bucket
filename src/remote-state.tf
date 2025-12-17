efa0720fc89685facf9d10037893c55444cf57e6
Dan Miller <miller0daniel@gmail.com>
2025-12-17 14:30:37 -0500

    feat: add account_map_enabled variable for static account mapping (#67)
    * feat: add account_map_enabled variable for static account mapping
    
    * fix: move account_map variables to providers.tf for override support

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -3,9 +3,12 @@ module "account_map" {
   version = "1.8.0"
 
   component   = var.account_map_component_name
-  environment = var.account_map_environment_name
-  stage       = var.account_map_stage_name
-  tenant      = var.account_map_tenant_name
+  environment = var.account_map_enabled ? var.account_map_environment_name : ""
+  stage       = var.account_map_enabled ? var.account_map_stage_name : ""
+  tenant      = var.account_map_enabled ? var.account_map_tenant_name : ""
+
+  bypass   = !var.account_map_enabled
+  defaults = var.account_map
 
   context = module.this.context
 }
